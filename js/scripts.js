a0ef2c7e54a562100571d6d6380b753920b43da9
Danielle Roberson <danielle.roberson@icloud.com>
2017-05-02 00:31:03 -0700

    Level 5 fin

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const panels = document.querySelectorAll('.panel');
+function toggleOpen(){
+    this.classList.toggle('open');  
+    this.nextElementSibling.classList.remove('open');
+    this.previousElementSibling.classList.remove('open');
+
+
+}
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+    
+}
+
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+
